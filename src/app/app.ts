754a937796a157fd10f04f001f208946aec97567
Trickypie <klimova.pr@gmail.com>
2023-02-03 23:01:30 +0300

    feat: add testing map on page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,32 @@
-import Footer from '../components/footer/footer';
-import Header from '../components/header/header';
+import BaseComponent from '../components/base-component/base-component';
+/* import Footer from '../components/footer/footer';
+import Header from '../components/header/header'; */
 
 export default class App {
-  private header: Header = new Header(this.parent);
+  /* private header: Header = new Header(this.parent); */
 
-  private footer: Footer = new Footer(this.parent);
+  private mapDiv: BaseComponent<'div'> = new BaseComponent('div', this.parent, 'map', '', {
+    id: 'map',
+    style: 'height: 50vh',
+  });
+
+  /* private footer: Footer = new Footer(this.parent); */
 
   constructor(private readonly parent: HTMLElement) {
     this.parent.classList.add('root');
+    App.addKey(this.parent);
   }
 
   public init(): void {
     this.parent.style.backgroundColor = 'grey';
+    this.parent.style.height = '100%';
+  }
+
+  public static addKey(parent: HTMLElement): BaseComponent<'script'> {
+    const script = new BaseComponent('script', parent, '', '', {
+      async: '',
+      src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC90BCUHG7PI6cW9XNex-5bY3Dd44Rqhgs&callback=initMap',
+    });
+    return script;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,21 @@ import App from './app/app';
 const app = new App(document.body);
 
 app.init();
+
+function initMap(): void {
+  const mapWrapper: HTMLElement | null = document.getElementById('map');
+  if (mapWrapper) {
+    const map = new google.maps.Map(mapWrapper, {
+      zoom: 8,
+      center: { lat: 40, lng: 0 },
+    });
+    console.log(map);
+  }
+}
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
