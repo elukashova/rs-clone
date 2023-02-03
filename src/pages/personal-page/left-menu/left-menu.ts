363c7ece257893e108ae969529f373126e185c09
matthewTheWizzard <mmdrumnew@gmail.com>
2023-02-03 23:47:36 +0400

    feat: add training journal

--- src/pages/personal-page/left-menu/left-menu.ts
+++ src/pages/personal-page/left-menu/left-menu.ts
@@ -1,8 +1,11 @@
 import BaseComponent from '../../../components/base-component/base-component';
 import ProfileCard from './profile-card/profile-card';
+import TrainingJournal from './training-journal/training-journal';
 
 export default class LeftMenu extends BaseComponent<'aside'> {
   public profileCard = new ProfileCard(this.element, '', 'new user', 'here would be your description');
+
+  public trainingJournal = new TrainingJournal(this.element);
 
   constructor(parent: HTMLElement) {
     super('aside', parent);
--- src/pages/personal-page/left-menu/profile-card/profile-card.ts
+++ src/pages/personal-page/left-menu/profile-card/profile-card.ts
@@ -3,7 +3,7 @@ import BaseComponent from '../../../../components/base-component/base-component'
 import ProfileInfo from './profile-card-info';
 
 export default class ProfileCard extends BaseComponent<'div'> {
-  private photo: BaseComponent<'div'> = new BaseComponent('div', this.element, 'profile-card-photo');
+  private photo: BaseComponent<'div'> = new BaseComponent('div', this.element, 'profile-card__photo');
 
   private name: BaseComponent<'h4'> | undefined;
 
@@ -18,15 +18,15 @@ export default class ProfileCard extends BaseComponent<'div'> {
   public trainings: ProfileInfo = new ProfileInfo('тренировки');
 
   constructor(parent: HTMLElement, photo: string, name: string, about: string) {
-    super('div', parent);
+    super('div', parent, 'profile-card');
     this.render(photo, name, about);
   }
 
   private render(photo: string, name: string, about: string): void {
     this.photo.element.style.background = `url${photo}`;
-    this.name = new BaseComponent('h4', this.element, 'profile-card-name', name);
-    this.about = new BaseComponent('p', this.element, 'profile-card-about', about);
-    this.profileScore = new BaseComponent('div', this.element, 'profile-card-info');
+    this.name = new BaseComponent('h4', this.element, 'profile-card__name', name);
+    this.about = new BaseComponent('p', this.element, 'profile-card__about', about);
+    this.profileScore = new BaseComponent('div', this.element, 'profile-card__info');
     // eslint-disable-next-line prettier/prettier
     this.profileScore.element.append(
       this.folowers.element,
