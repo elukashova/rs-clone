3a724a1053d9fa13b12c4893eeaab5b31eb4724a
elukashova <ele.lukashova@gmail.com>
2023-02-10 23:14:48 +0100

    refactor: move svg types into svg folder

---
+++ src/components/base-component/svg/svg.types.ts
@@ -0,0 +1,28 @@
+enum SvgNames {
+  Search = 'search',
+  Settings = 'settings',
+  PaginationLeft = 'pagination-left',
+  PaginationRight = 'pagination-right',
+  ThreeDots = 'three-dots',
+  Update = 'update',
+  Burger = 'burger-menu',
+  Copy = 'copy',
+  Edit = 'edit',
+  Star = 'star',
+  Favorite = 'favorite',
+  MapMarker = 'map-marker',
+
+  Plus = 'plus',
+  Plus2 = 'plus2',
+  Plus3 = 'icons-plus',
+  Plus4 = 'icons-plus-black',
+  Check = 'check',
+  Close = 'close',
+
+  Hiking = 'hiking',
+  Running = 'running',
+  Walking = 'walking',
+  Cycling = 'cycling',
+}
+
+export default SvgNames;
