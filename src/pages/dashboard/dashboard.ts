4c1e2bd62c9d5300d5a7bc4ffe78d8d64dd9b24c
matthewTheWizzard <mmdrumnew@gmail.com>
2023-02-11 23:44:45 +0400

    feat: add-activity-class

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -4,8 +4,33 @@ import LeftMenu from './left-menu/left-menu';
 import TrainingFeed from './training-feed/training-feed';
 import RightMenu from './right-menu/right-menu';
 import { getUser } from '../../app/loader/services/user-services';
-import { Token, User } from '../../app/loader/loader.types';
+import { SportType, Token, User } from '../../app/loader/loader.types';
 import { checkDataInLocalStorage, setDataToLocalStorage } from '../../utils/local-storage';
+import Post from './training-feed/post/post';
+
+const mockDataUser = {
+  avatarUrl: './assets/images/avatars/avatar9.png',
+  bio: 'I am Batman',
+  country: 'unknown',
+  email: 'noneofyourbuisiness@nope.com',
+  id: '2',
+  username: 'Michael Knowles',
+};
+
+const mockDataActivity = {
+  id: 1,
+  user_id: '1',
+  user: mockDataUser,
+  sport: SportType.HIKING,
+  title: 'Пешком по пляжу',
+  time: '20min',
+  distance: '100m',
+  elevation: 1000,
+  description: 'my training was quite good',
+  created_at: new Date(),
+  updated_at: new Date(),
+  location: 'string',
+};
 
 export default class Dashboard extends BaseComponent<'section'> {
   private leftMenu!: LeftMenu;
@@ -22,6 +47,7 @@ export default class Dashboard extends BaseComponent<'section'> {
     super('section', undefined, 'dashboard section');
     if (this.token) {
       getUser(this.token).then((user: User) => {
+        this.trainingFeed.element.append(new Post(mockDataActivity).element); // добавляем один пост
         this.currentUser = {
           ...user,
         };
