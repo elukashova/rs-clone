368154ee6bf3e3ba8ae4387a4be3a83d3394e86f
elukashova <ele.lukashova@gmail.com>
2023-02-11 21:23:13 +0100

    feat: add auto resizing to textareas

--- src/components/base-component/textarea/editable-textarea.types.ts
+++ src/components/base-component/textarea/editable-textarea.types.ts
@@ -1,6 +1,16 @@
-enum TextareaTypes {
+export enum TextareaTypes {
   Username = 'username',
   Bio = 'bio',
 }
 
-export default TextareaTypes;
+export enum TextareaLength {
+  Default = '50',
+  Bio = '150',
+}
+
+export enum TextareaColsNumber {
+  DefaultName = 22,
+  DefaultBio = 35,
+  IsUpdateName = 19,
+  IsUpdateBio = 30,
+}
