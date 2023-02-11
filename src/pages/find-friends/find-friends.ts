c5567f841ad954826b8495b8357939fa9746dad1
Trickypie <klimova.pr@gmail.com>
2023-02-11 22:24:53 +0300

    feat: do layout for find friends class

--- src/pages/find-friends/find-friends.ts
+++ src/pages/find-friends/find-friends.ts
@@ -1,23 +1,30 @@
 import './find-friends.css';
+import users from './user-test';
 import BaseComponent from '../../components/base-component/base-component';
-import { getUser } from '../../app/loader/services/user-services';
 import { User, Token } from '../../app/loader/loader.types';
 import { checkDataInLocalStorage } from '../../utils/local-storage';
-import Input from '../../components/base-component/text-input-and-label/text-input';
 import { ProjectColors } from '../../utils/consts';
+import NotFriend from './not-friend/not-friend';
+import Input from '../../components/base-component/text-input-and-label/text-input';
 import SvgNames from '../../components/base-component/svg/svg.types';
+import { UserData } from './type-not-friend';
+import Friend from './friend/friend';
 
 export default class Friends extends BaseComponent<'section'> {
+  public notFriendsAll: NotFriend[] = [];
+
+  public friendsAll: Friend[] = [];
+
   private findingContainer = new BaseComponent('div', this.element, 'find-friends__container');
 
   private notFriendsBlock = new BaseComponent('div', this.findingContainer.element, 'not-friends__block');
 
-  private notFriendsTitle = new BaseComponent('h3', this.notFriendsBlock.element, 'not-friends__title', 'Найти друзей');
+  private notFriendsTitle = new BaseComponent('h3', this.notFriendsBlock.element, 'not-friends__title', 'Find friends');
 
   private notFriendsSearch = new Input(
     this.notFriendsBlock.element,
-    'not-friends__input input-search',
-    'Имя спортсмена',
+    'not-friends__input-search input-search',
+    'Sportsman name',
     {
       type: 'search',
     },
@@ -25,7 +32,11 @@ export default class Friends extends BaseComponent<'section'> {
 
   private friendsBlock = new BaseComponent('div', this.findingContainer.element, 'friends__block');
 
-  private friendsTitle = new BaseComponent('h3', this.friendsBlock.element, 'friends__title', 'Мои подписки');
+  private friendsTitle = new BaseComponent('h3', this.friendsBlock.element, 'friends__title', 'My Subscriptions');
+
+  private friendsSearch = new Input(this.friendsBlock.element, 'friends__input-search input-search', 'Sportsman name', {
+    type: 'search',
+  });
 
   private token: Token | null = checkDataInLocalStorage('userSessionToken');
 
@@ -33,25 +44,42 @@ export default class Friends extends BaseComponent<'section'> {
 
   constructor(parent: HTMLElement) {
     super('section', parent, 'find-friends find-friends-section');
-    if (this.token) {
+    /* if (this.token) {
       getUser(this.token).then((user) => {
         console.log(user);
-        /* this.currentUser = {
+        this.currentUser = {
           ...user,
-        }; */
-
-        /* type User = {
-  avatarUrl: string;
-  bio: string;
-  country: string;
-  created_at: string;
-  email: string;
-  id: string;
-  updated_at: string;
-  username: string;
-}; */
+        };
       });
-    }
-    this.notFriendsSearch.addSvgIcon(SvgNames.Search, ProjectColors.Turquoise, 'search');
+    } */
+    this.renderPage(users);
+    this.notFriendsSearch.addSvgIcon(SvgNames.Search, ProjectColors.Grey, 'search');
+    this.friendsSearch.addSvgIcon(SvgNames.Search, ProjectColors.Grey, 'search');
+  }
+
+  private renderPage(data: UserData[]): void {
+    data.forEach((user, index) => {
+      const notFriend = new NotFriend(
+        this.notFriendsBlock.element,
+        'not-friends__element',
+        {
+          id: `not-friends-${index}`,
+        },
+        user,
+      );
+      this.notFriendsAll.push(notFriend);
+    });
+
+    data.forEach((user, index) => {
+      const friend = new Friend(
+        this.friendsBlock.element,
+        'friends__element',
+        {
+          id: `friends-${index}`,
+        },
+        user,
+      );
+      this.friendsAll.push(friend);
+    });
   }
 }
